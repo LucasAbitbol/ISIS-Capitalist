ee700371cebd8a55948b2793cb2590f658db2100
LucasAbitbol <lucasabitbol6@gmail.com>
2022-03-08 09:40:25 +0100

    Fonctions PUT manager/product

--- src/main/java/com/example/ISISCapitalist/Services.java
+++ src/main/java/com/example/ISISCapitalist/Services.java
@@ -51,4 +51,79 @@ public class Services {
     World getWorld(String username) {
         return readWorldFromXml(username);
     }
+
+    public ProductType findProductById(World world, int id) {
+        for (ProductType p : world.getProducts().getProduct()) {
+            if (p.getId() == id) {
+                return p;
+            }
+        }
+        return null;
+    }
+
+
+    public PallierType findManagerByName(World world, String managerName){
+        for (PallierType m : world.getManagers().getPallier()) {
+            if (m.getName()==managerName) {
+                return m;
+            }
+        }
+        return null;
+    }
+
+    double getCostOfProduct(ProductType product, int qte) {
+        double costProduct = product.getCout();
+        return (costProduct * (1-Math.pow(product.getCroissance(), qte))) / (1-product.getCroissance());
+    }
+
+    public Boolean updateProduct(String username, ProductType newproduct) {
+        World world = getWorld(username);
+        ProductType product = findProductById(world, newproduct.getId());
+        if (product == null) {
+            return false;
+        }
+        // calculer la variation de quantité. Si elle est positive c'est
+        // que le joueur a acheté une certaine quantité de ce produit
+        // sinon c’est qu’il s’agit d’un lancement de production.
+        int qtchange = newproduct.getQuantite() - product.getQuantite();
+        if (qtchange > 0) {
+        // soustraire de l'argent du joueur le cout de la quantité achetée et mettre à jour la quantité de product
+            world.setMoney(world.getMoney()-(getCostOfProduct(product, qtchange)));
+            product.setCout(product.getCout()*Math.pow(product.getCroissance(),qtchange));
+            product.setQuantite(product.getQuantite());
+        }
+        else {
+        // initialiser product.timeleft à product.vitesse // pour lancer la production
+            product.setTimeleft(product.getVitesse());
+        }
+        // sauvegarder les changements du monde
+        saveWorldToXml(world, username);
+        return true;
+    }
+
+    // prend en paramètre le pseudo du joueur et le manager acheté.
+    // renvoie false si l’action n’a pas pu être traitée
+    public Boolean updateManager(String username, PallierType newmanager) {
+        // aller chercher le monde qui correspond au joueur
+        World world = getWorld(username);
+        // trouver dans ce monde, le manager équivalent à celui passé
+        // en paramètre
+        PallierType manager = findManagerByName(world, newmanager.getName());
+        if (manager == null) {
+            return false;
+        }
+        // débloquer ce manager
+        // trouver le produit correspondant au manager
+        manager.setUnlocked(true);
+        ProductType product = findProductById(world, manager.getIdcible());
+        if (product == null) {
+            return false;
+        }
+        // débloquer le manager de ce produit
+        // soustraire de l'argent du joueur le cout du manager
+        // sauvegarder les changements au monde
+        product.setManagerUnlocked(true);
+        saveWorldToXml(world, username);
+        return true;
+    }
 }
--- src/main/java/com/example/ISISCapitalist/WebService.java
+++ src/main/java/com/example/ISISCapitalist/WebService.java
@@ -1,6 +1,6 @@
 package com.example.ISISCapitalist;
 
-import com.example.world.World;
+import com.example.world.*;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -16,9 +16,20 @@ public class WebService {
 
     @GetMapping(value = "world", produces = {"application/xml", "application/json"})
 
-    public ResponseEntity<World> getWorld(@RequestHeader(value = "X-User",
-            required = false) String username) {
+    public ResponseEntity<World> getWorld(@RequestHeader(value = "X-User", required = false) String username) {
         World world = services.getWorld(username);
         return ResponseEntity.ok(world);
+    }
+
+    @PutMapping(value = "/product", consumes = "application/json")
+    ResponseEntity<World> newproduct(String username, ProductType product) {
+        services.updateProduct(username, product);
+        return ResponseEntity.ok(services.getWorld(username));
+    }
+
+    @PutMapping(value = "/manager", consumes = "application/json")
+    ResponseEntity<World> newmanager(String username, PallierType manager) {
+        services.updateManager(username, manager);
+        return ResponseEntity.ok(services.getWorld(username));
     }
 }
